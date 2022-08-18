f2127174ae058761a63919b25242d1764f47eb99
Sana-Amin <amin.sana19@gmail.com>
2022-08-18 11:49:23 -0500

    first commit

---
+++ src/Mars.java
@@ -0,0 +1,44 @@
+public class Mars {
+    public static void main(String[] args) throws InterruptedException {
+        String colonyName = "redTown";
+        int shipPopulation = 300;
+        double shipFood = 4000.00;
+        boolean landing = true;
+        landing = landingCheck(100);
+
+        shipFood = (shipFood - (shipPopulation * 0.75)) * 2;
+        System.out.println("The amount of food that is left is:" + shipFood);
+
+        double extraCrate = .50;
+
+        shipFood = shipFood + (shipFood * extraCrate);
+
+        shipPopulation = shipPopulation + 5;
+
+        String landingLocation = "The Ocean";
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzz Landing on the Plain");
+        } else {
+            System.out.println("Error!!!! Flight plan already set. Landing on the Plain");
+        }
+    }
+
+        public static boolean landingCheck(int loops) throws InterruptedException {
+            for (int i =0; i <= loops; i++){
+                if(i % 3 ==0 && i % 5 ==0){
+                    System.out.println("Keep Center");
+                }else if(i % 5 == 0){
+                    System.out.println("Right");
+                }else{
+                    System.out.println("Calculating");
+                }
+                Thread.sleep(250);
+            }
+            System.out.println("landed");
+
+            return false;
+
+        }
+
+
+}
